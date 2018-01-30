5f6b078858cfa5e352f6755835b53e57768d588b
Chris <chris.anthony.yee@gmail.com>
2018-01-30 14:37:21 -0800

    User Authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,9 +79,18 @@ end
 end
 sponsored_posts = SponsoredPost.all
 
-user = User.first
- user.update_attributes!(
-   email: 'chris.anthony.yee@gmail.com', 
+# Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
 
