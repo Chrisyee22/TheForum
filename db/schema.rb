a2488a239bdb3134bf74f4a6afc69d81e446c377
Chris <chris.anthony.yee@gmail.com>
2018-01-29 22:53:58 -0800

    Added users and refactored

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117090246) do
+ActiveRecord::Schema.define(version: 20180130012455) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -43,8 +43,10 @@ ActiveRecord::Schema.define(version: 20180117090246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "topic_id"
+    t.integer "user_id"
     t.index ["post_id"], name: "index_posts_on_post_id"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'random_data'
 
+# Create Users
+ 5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
 # Create Topics
  15.times do
    Topic.create!(
@@ -19,6 +30,7 @@ require 'random_data'
 #create posts
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -67,7 +79,14 @@ end
 end
 sponsored_posts = SponsoredPost.all
 
+user = User.first
+ user.update_attributes!(
+   email: 'chris.anthony.yee@gmail.com', 
+   password: 'helloworld'
+ )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts " #{Comment.count} comments created"
