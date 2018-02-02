826649bd19fbff10c808d1a893296620c404e7be
Chris <chris.anthony.yee@gmail.com>
2018-02-01 16:45:50 -0800

    added ranking

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,12 +29,15 @@ require 'random_data'
 
 #create posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ # #13
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 posts = Post.all
 
@@ -97,6 +100,7 @@ sponsored_posts = SponsoredPost.all
  )
 
 puts "Seed finished"
+puts "#{Vote.count} votes created"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
